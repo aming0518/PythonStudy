4cc99331235720f8b6b355a58f459f2fe5c261ef
aming0518 <835423419@qq.com>
2019-10-09 12:38:43 +0800

    PythonStudyDay6

---
+++ print.py
@@ -0,0 +1,6 @@
+print("a",end="") #end默认是\n换行，end=“ ”空格 可以打印空格
+print("a",end="")
+print("a",end="")
+
+
+print(1,2,3,sep="#")  #print连续打印时默认空格间隔，也可以使用其他间隔
---
+++ str.py
@@ -0,0 +1,4 @@
+import os
+os.system('calc')  #单引号双引号等价
+os.system("calc")  #三引号可以换行输入
+os.system('''calc''')
